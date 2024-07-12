dc75d077eac05e98ced4f760beadbcc179026d58
Punyakrit Singh Makhni <punyakritsinghmakhni@gmail.com>
2024-07-12 22:22:00 +0530

    shadcn setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       jendjemdnjke
+      <Button>djsn</Button>
     </div>
   );
 }
---
+++ components/reusable/AppBar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AppBar() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default AppBar
